a7428e547b73f29b0cc79dd1944d849f85deaeff
Yuanxuan Wang <zellux@gmail.com>
2012-01-13 19:33:19 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paperoo::Application.config.secret_token = '75272f588eccfaaebfe27cf55c9cd2e275d9abfcb46ef65d6f64a6c8fb09bdeacf4c1cdfe0d6aecead59642e5e653dff23ea0d2915fd12546f9682e408d89751'
